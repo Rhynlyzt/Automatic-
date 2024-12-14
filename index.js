ec6401c8e87a9b79865ac4ac98bd432b85d6c6a5
Rhynlyzt <166503046+Rhynlyzt@users.noreply.github.com>
2024-12-14 15:35:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -210,7 +210,7 @@ app.post('/login', async (req, res) => {
 });
 app.listen(3000, () => {
   console.log(
-`⣿⣿⣿█════╝░`);
+`Running`);
 });
 process.on('unhandledRejection', (reason) => {
   console.error('Unhandled Promise Rejection:', reason);
